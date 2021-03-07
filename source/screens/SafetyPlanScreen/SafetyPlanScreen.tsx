fe15b9ecf87f1886bbc85256ae4ecb3755c209aa
Adrian Lacy <adrianlacy01@gmail.com>
2021-03-07 15:47:37 -0600

    added feature buttons to safety plan screen

--- source/screens/SafetyPlanScreen/SafetyPlanScreen.tsx
+++ source/screens/SafetyPlanScreen/SafetyPlanScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import styles from './styles'
 
 export default ()=>
 {
     return (
-        <Text>Safety Plan Screen</Text>
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button}><Text style={styles.buttonTitle}>Enter Your Warning Signs</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.buttonTitle}>Enter Coping Strategies</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.buttonTitle}>Enter Favorite Social Activites</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.buttonTitle}>People I Can Ask For Help</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.buttonTitle}>Emergency Services</Text></TouchableOpacity>
+        </View>
     )
 
 }
---
+++ source/screens/SafetyPlanScreen/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    button: {
+        marginLeft: 0,
+        marginRight: 0,
+        marginTop: 40,
+        height: 48,
+        width:300,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: 'center',
+        backgroundColor:'blue',
+    },
+    buttonTitle: {
+        color: 'white',
+        fontSize: 22,
+        fontWeight: "bold",
+        justifyContent:'center',
+    },
+    container: {
+        alignItems: 'center',
+        backgroundColor:'#219ebc',
+        flex:1
+    },
+
+})
